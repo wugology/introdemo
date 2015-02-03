1b7e82366c643bec3ad65908294714c0675726d7
Daniel W. Hieber <dwhieb@users.noreply.github.com>
2015-02-02 21:28:09 -0800

    Fixed white space, minor code changes

--- js/demo.js
+++ js/demo.js
@@ -1,70 +1,81 @@
 // GLOBAL VARIABLES
 var nodes = {
-	downloadArea: document.querySelector('#downloadArea'),
-	inputArea: document.querySelector('#inputArea'),
-	jsonArea: document.querySelector('#jsonArea'),
-	transcriptionBox: document.querySelector('#transcriptionBox'),
-	translationBox: document.querySelector('#translationBox')
+  downloadArea: document.querySelector('#downloadArea'),
+  inputArea: document.querySelector('#inputArea'),
+  jsonArea: document.querySelector('#jsonArea'),
+  transcriptionBox: document.querySelector('#transcriptionBox'),
+  translationBox: document.querySelector('#translationBox')
 };
 
+// The phrase object
 var phrase = {
-	transcription: '',
-	translation: '',
-	words: []
+  transcription: '',
+  translation: '',
+  words: []
 };
 
 // CONSTRUCTORS
 function Word(token, gloss, partOfSpeech) {
-	this.token = token;
-	this.gloss = gloss;
-	this.partOfSpeech = partOfSpeech;
+  this.token = token;
+  this.gloss = gloss;
+  this.partOfSpeech = partOfSpeech;
+  this.words = [];
 };
 
 // GLOBAL FUNCTIONS
+// Displays the dictionary visualization, using data from the phrase object
 function displayDictionary() {
-	
+  
 };
 
+// Displays the interlinear gloss visualization, using data from the phrase object
 function displayInterlinear() {
-	
+  
 };
 
+// Displays the JSON visualization, using data from the phrase object
 function displayJSON() {	
-	nodes.jsonArea.innerHTML = JSON.stringify(phrase, null, 2);
+  nodes.jsonArea.innerHTML = JSON.stringify(phrase, null, 2);
 };
 
+// Creates a download button where users can download their JSON data
 function downloadJSON(){
-	// Get the JSON data
-	var text = nodes.jsonArea.innerHTML;
-	// Encode
-	var data = "text/json;charset=utf-8," + encodeURIComponent(text);
-	// Create a download link
-	nodes.downloadArea.innerHTML = '<a class="button blue" href="data:' + data + '" download="data.json">Download your data!</a>'
-}
+  // Get the JSON data
+  var text = nodes.jsonArea.innerHTML;
+  // Encode
+  var data = "text/json;charset=utf-8," + encodeURIComponent(text);
+  // Create a download link
+  nodes.downloadArea.innerHTML = '<a class="button blue" href="data:' + data + '" download="data.json">Download your data!</a>'
+};
 
 // Updates the phrase object, and (re-)renders the JSON, interlinear, and lexicon visualizations
 function render() {
-	updatePhrase();
-	displayJSON();
-	displayInterlinear();
-	displayDictionary();
+  updatePhrase();
+  displayJSON();
+  displayInterlinear();
+  displayDictionary();
 };
 
+// Whenever the user types/changes the data, the phrase object is updated with that data
 function updatePhrase() {
-	var transcription = nodes.transcriptionBox.value;
-	var translation = nodes.translationBox.value;
-    var tokens = transcription.split(/[ .,!?\:\;\/\\\[\]\{\}\<\>\-\u3000\u00A1\u00BF\u0022\u0027\u00AB\u00BB\u2018\u2019\u201A\u201B\u201C\u201D\u201E\u201F\u2039\u203A\u300C\u300D\u300E\u300F\u301D\u301E\u301F\uFE41\uFE42\uFE43\uFE44\uFF02\uFF07\uFF62\uFF63]+/g).filter(Boolean);
-	// basics, brackets, inverted !/?, various quotes from around the world
-    // the filter(Boolean) will filter out the "" ending split since "" == False
-	var words = [];
-	tokens.forEach(function(token) {
-		var word = new Word(token, '', '');
-		words.push(word);
-	});
-	phrase.words = words;
-	
-	phrase.transcription = transcription;
-	phrase.translation = translation;
+  // Gets the data that users have typed into the Transcription and Translation boxes
+  var transcription = nodes.transcriptionBox.value;
+  var translation = nodes.translationBox.value;
+  
+  // Tokenizes the transcription based on punctuation and other funky characters
+    // Includes basic punctuation, brackets, inverted !/?, various quotes from around the world
+    // The filter(Boolean) will filter out the "" ending split since "" == False
+  var tokens = transcription.split(/[ .,!?\:\;\/\\\[\]\{\}\<\>\-\u3000\u00A1\u00BF\u0022\u0027\u00AB\u00BB\u2018\u2019\u201A\u201B\u201C\u201D\u201E\u201F\u2039\u203A\u300C\u300D\u300E\u300F\u301D\u301E\u301F\uFE41\uFE42\uFE43\uFE44\uFF02\uFF07\uFF62\uFF63]+/g).filter(Boolean);
+  
+  // For each token, creates a new word, then adds that word to the phrase object
+  tokens.forEach(function(token) {
+    var word = new Word(token, '', '');
+    phrase.words.push(word);
+  });
+  
+  // Sets the transcription and translation properties of the phrase object equal to the data typed in the transcription and translation boxes
+  phrase.transcription = transcription;
+  phrase.translation = translation;
 };
 
 // EVENT LISTENERS
