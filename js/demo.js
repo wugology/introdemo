af59a71c53625b1d4595395cd58d2471d65d89d1
Daniel W. Hieber <dwhieb@gmail.com>
2015-02-03 22:27:38 -0800

    Fixed updatePhrase()
    The words array has to be fully populated before it gets copied onto the
    phrase. You can't just push the words directly to the phrase. Fixes #24.

--- js/demo.js
+++ js/demo.js
@@ -66,12 +66,15 @@ function updatePhrase() {
     // Includes basic punctuation, brackets, inverted !/?, various quotes from around the world
     // The filter(Boolean) will filter out the "" ending split since "" == False
   var tokens = transcription.split(/[ .,!?\:\;\/\\\[\]\{\}\<\>\-\u3000\u00A1\u00BF\u0022\u0027\u00AB\u00BB\u2018\u2019\u201A\u201B\u201C\u201D\u201E\u201F\u2039\u203A\u300C\u300D\u300E\u300F\u301D\u301E\u301F\uFE41\uFE42\uFE43\uFE44\uFF02\uFF07\uFF62\uFF63]+/g).filter(Boolean);
-  
+  console.log(tokens);
+	
   // For each token, creates a new word, then adds that word to the phrase object
+	var words = [];
   tokens.forEach(function(token) {
     var word = new Word(token, '', '');
-    phrase.words.push(word);
+    words.push(word);
   });
+	phrase.words = words;
   
   // Sets the transcription and translation properties of the phrase object equal to the data typed in the transcription and translation boxes
   phrase.transcription = transcription;
