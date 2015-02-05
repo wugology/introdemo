b8e01776a820e16ad4d9febbfacd26daba72ba14
Daniel W. Hieber <dwhieb@gmail.com>
2015-02-05 11:42:05 -0800

    Add word glosses functionality to inputArea
    * User can input text into a contenteditable word gloss area
    * If there is no word gloss yet, or if the user deletes it, placeholder
    text is entered into the gloss area
    * The phrase object updates when any gloss is edited
    * Other minor code refactoring

--- js/demo.js
+++ js/demo.js
@@ -6,10 +6,16 @@ var nodes = {
   jsonArea: document.querySelector('#jsonArea'),
   transcriptionBox: document.querySelector('#transcriptionBox'),
   translationBox: document.querySelector('#translationBox'),
-  wordTemplate: document.querySelector('#wordTemplate')
+  wordTemplate: document.querySelector('#wordTemplate'),
+  wordsWrapper: document.querySelector('.words')
 };
 
-// The phrase object (NOT created from the Phrase() constructor, for pedagogical purposes)
+// Defines the delimiters that will be used to tokenize the string
+  // Includes basic punctuation, brackets, inverted !/?, various quotes from around the world
+var delimiters = [' ', '.', ',', '!', '?', ':', ';', '/', '\\', '[', '\\]', '{', '}', '<', '>', '-', '\u3000', '\u00A1', '\u00BF', '\u0022', '\u0027', '\u00AB', '\u00BB', '\u2018', '\u2019', '\u201A', '\u201B', '\u201C', '\u201D', '\u201E', '\u201F', '\u2039', '\u203A', '\u300C', '\u300D', '\u300E', '\u300F', '\u301D', '\u301E', '\u301F', '\uFE41', '\uFE42', '\uFE43', '\uFE44', '\uFF02', '\uFF07', '\uFF62', '\uFF63'];
+var placeholderText = '(enter gloss here)';
+
+// The phrase object (NOT created from the Phrase() constructor; this one is just for pedagogical purposes)
 var phrase = {
   transcription: '',
   translation: '',
@@ -25,13 +31,6 @@ function Word(token, gloss, partOfSpeech) {
 };
 
 // GLOBAL FUNCTIONS
-// Empties the content of a contenteditable element when the user clicks on it
-function clearContentEditable(ev) {
-  if (ev.target.className === 'wordGloss') {
-    ev.target.innerHTML = '';
-  }
-};
-
 // Displays the dictionary visualization, using data from the phrase object
 function displayDictionary() {
   
@@ -43,19 +42,25 @@ function displayInterlinear() {
 };
 
 // Displays the JSON visualization, using data from the phrase object
-function displayJSON() {  
+function displayJSON() {
   nodes.jsonArea.innerHTML = JSON.stringify(phrase, null, 2);
 };
 
 // Displays the words within the inputArea, so that users can add glosses to the words
-  // This is not an instance of the sitewide displayPhrase() function, for pedagogical purposes
+  // This is not an instance of the sitewide displayPhrase() function; this one is just for pedagogical purposes
 function displayPhrase() {
   var wordObject = wordTemplate.content.querySelector('.word');
   var wordToken = wordTemplate.content.querySelector('.wordToken');
+  var wordGloss = wordTemplate.content.querySelector('.wordGloss');
   nodes.inputAreaWordsWrapper.innerHTML = '';
   phrase.words.forEach(function(word, i) {
     wordObject.id = 'word_' + i;
     wordToken.innerHTML = word.token;
+    if (word.gloss === '') {
+      wordGloss.textContent = placeholderText;
+    } else {
+      wordGloss.textContent = word.gloss;
+    }
     var newWord = wordTemplate.content.cloneNode(true);
     nodes.inputAreaWordsWrapper.appendChild(newWord);
   });
@@ -71,9 +76,12 @@ function setDownloadButton(){
 
 // Updates the phrase object, and (re-)renders the JSON, interlinear, and lexicon visualizations
 function render(ev) {
-  updatePhrase(ev);
-  if (ev.target.className !== 'wordGloss' && ev.target.className !== 'wordPOS') {
-    displayPhrase();
+  updatePhrase();
+  if (ev.target.className !== 'wordGloss') {
+    displayPhrase(); 
+  }
+  if (ev.target.className === 'wordGloss' && ev.target.textContent === '') {
+    ev.target.textContent = placeholderText;
   }
   displayJSON();
   displayInterlinear();
@@ -82,38 +90,38 @@ function render(ev) {
 };
 
 // Whenever the user types/changes the data, the phrase object is updated with that data
-function updatePhrase(ev) {
-  if (ev.target.className === 'wordGloss') {
-    var wordIndex = ev.target.parentNode.id.replace('word_', '');
-    phrase.words[wordIndex].gloss = ev.target.innerHTML;
-  };
-  
+function updatePhrase() {
   // Gets the data that users have typed into the Transcription and Translation boxes
   var transcription = nodes.transcriptionBox.value;
   var translation = nodes.translationBox.value;
-  if (ev.target.id === 'transcriptionBox') {
-    // Defines the delimiters that will be used to tokenize the string
-      // Includes basic punctuation, brackets, inverted !/?, various quotes from around the world
-    var delimiters = [' ', '.', ',', '!', '?', ':', ';', '/', '\\', '[', '\\]', '{', '}', '<', '>', '-', '\u3000', '\u00A1', '\u00BF', '\u0022', '\u0027', '\u00AB', '\u00BB', '\u2018', '\u2019', '\u201A', '\u201B', '\u201C', '\u201D', '\u201E', '\u201F', '\u2039', '\u203A', '\u300C', '\u300D', '\u300E', '\u300F', '\u301D', '\u301E', '\u301F', '\uFE41', '\uFE42', '\uFE43', '\uFE44', '\uFF02', '\uFF07', '\uFF62', '\uFF63'];
 
-    // Tokenizes the transcription based on the delimiters
-    var tokens = tokenize(transcription, delimiters);
-
-    // For each token, creates a new word, then adds that word to the phrase object
-    var words = [];
-    tokens.forEach(function(token) {
-      var word = new Word(token, '', '');
-      words.push(word);
-    });
-    phrase.words = words;    
-  }
-    
   // Sets the transcription and translation properties of the phrase object equal to the data typed in the transcription and translation boxes
   phrase.transcription = transcription;
   phrase.translation = translation;
+
+  // Tokenizes the transcription based on the delimiters
+  var tokens = tokenize(transcription, delimiters);
+
+  // For each token, creates a new word, then adds that word to the phrase object
+  var words = [];
+  tokens.forEach(function(token) {
+    var word = new Word(token, '', '');
+    words.push(word);
+  });
+  phrase.words = words;    
+  
+  // Goes through each word element, gets the glosses, and stores them in the phrase
+    // This function must come after the tokenization above, so that the word objects already exist
+  var wordObjects = nodes.wordsWrapper.children;
+  for (var i=0; i<wordObjects.length; i++) {
+    var wordIndex = wordObjects[i].id.replace('word_', '');
+    var gloss = document.querySelector('#' + wordObjects[i].id + ' .wordGloss').textContent;
+    if (gloss !== placeholderText) {
+      phrase.words[wordIndex].gloss = gloss;
+    }
+  }
 };
 
 // EVENT LISTENERS
-nodes.inputArea.addEventListener('click', clearContentEditable);
 nodes.inputArea.addEventListener('input', render);
 window.addEventListener('load', render);
